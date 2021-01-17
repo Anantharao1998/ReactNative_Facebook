0d91d129230e0bdcbdaad48ae023fc62c485eabc
Anantha <anantharao1998@gmail.com>
2021-01-17 21:59:39 +0800

    Test commit 3

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native test2</Text>
+      <Text>Hello React Native test3</Text>
       <StatusBar style="auto" />
     </View>
   );
